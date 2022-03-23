d14c69f145e18dd56035f11001650ec706d42198
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-03-23 15:28:09 +0800

    FEATURE: Validate `QuestionAnswerComment#raw` with `TextSentinel`.

--- app/validators/question_answer_comment_validator.rb
+++ app/validators/question_answer_comment_validator.rb
@@ -11,5 +11,8 @@ class QuestionAnswerCommentValidator < ActiveModel::Validator
     StrippedLengthValidator.validate(
       record, :raw, record.raw, SiteSetting.min_post_length..SiteSetting.qa_comment_max_raw_length
     )
+
+    sentinel = TextSentinel.body_sentinel(record.raw)
+    record.errors.add(:raw, I18n.t(:is_invalid)) unless sentinel.valid?
   end
 end
