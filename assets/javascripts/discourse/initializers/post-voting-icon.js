041fffcf36d96b930e2f7cd7800c7d5e429c6e3d
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-11-16 00:01:06 +0200

    FIX: Register plugin notification item icon

---
+++ assets/javascripts/discourse/initializers/post-voting-icon.js
@@ -0,0 +1,16 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "post-voting-icon",
+
+  initialize(container) {
+    const siteSettings = container.lookup("site-settings:main");
+    if (!siteSettings.qa_enabled) {
+      return;
+    }
+
+    withPluginApi("1.2.0", (api) => {
+      api.replaceIcon("notification.question_answer_user_commented", "comment");
+    });
+  },
+};
