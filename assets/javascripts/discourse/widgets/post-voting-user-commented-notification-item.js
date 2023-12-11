9c571e51d707bbc7babdb8ff25de1834a692b574
Mark VanLandingham <markvanlan@gmail.com>
2023-12-11 08:32:26 -0600

    add widget back for now

---
+++ assets/javascripts/discourse/widgets/post-voting-user-commented-notification-item.js
@@ -0,0 +1,21 @@
+import { postUrl } from "discourse/lib/utilities";
+import { DefaultNotificationItem } from "discourse/widgets/default-notification-item";
+import { createWidgetFrom } from "discourse/widgets/widget";
+import { iconNode } from "discourse-common/lib/icon-library";
+import { buildAnchorId } from "../components/post-voting-comment";
+
+createWidgetFrom(
+  DefaultNotificationItem,
+  "question-answer-user-commented-notification-item",
+  {
+    url(data) {
+      const attrs = this.attrs;
+      const url = postUrl(attrs.slug, attrs.topic_id, attrs.post_number);
+      return `${url}#${buildAnchorId(data.post_voting_comment_id)}`;
+    },
+
+    icon() {
+      return iconNode("comment");
+    },
+  }
+);
