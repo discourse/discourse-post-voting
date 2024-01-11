a5b24d8dbd84143c4c1cbb7ae8df05a98031df00
Juan David Martinez <juan@discourse.org>
2024-01-11 16:20:45 -0500

    FIX: Customize New Button Compatibility with Post Voting Plugin

--- assets/javascripts/discourse/initializers/extend-composer-actions.js
+++ assets/javascripts/discourse/initializers/extend-composer-actions.js
@@ -5,6 +5,7 @@ import I18n from "I18n";
 
 export default {
   name: "extend-composer-actions",
+  after: "chat-setup",
   initialize(container) {
     const siteSettings = container.lookup("site-settings:main");
 
