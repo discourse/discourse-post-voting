005b58f9553e605d01f4becea976351120013084
Bianca Nenciu <nbianca@users.noreply.github.com>
2024-10-28 19:58:36 +0200

    DEV: Fix mismatched column types (#217)
    The primary key is usually a bigint column, but the foreign key columns
    are usually of integer type. This can lead to issues when joining these
    columns due to mismatched types and different value ranges.
    
    This was using a temporary plugin / test API to make tests pass, but
    it is safe to alter these tables because they usually have less than 1M
    rows.

--- spec/plugin_helper.rb
+++ spec/plugin_helper.rb
@@ -5,11 +5,4 @@ RSpec.configure do |config|
   # since the creation of some models require
   # the plugin to be turned on
   SiteSetting.post_voting_enabled = true
-
-  config.before(:suite) do
-    if defined?(migrate_column_to_bigint)
-      migrate_column_to_bigint(PostVotingCommentCustomField, :post_voting_comment_id)
-      migrate_column_to_bigint(PostVotingVote, :votable_id)
-    end
-  end
 end
