2d3ad2be95fc93f232c412ce250521e897ab5abc
David Taylor <david@taylorhq.com>
2024-11-18 13:49:28 +0000

    DEV: Update eslint config

--- assets/javascripts/discourse/components/post-voting-comment-actions.js
+++ assets/javascripts/discourse/components/post-voting-comment-actions.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import FlagModal from "discourse/components/modal/flag";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
