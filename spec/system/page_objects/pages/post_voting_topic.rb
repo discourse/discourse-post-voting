e7d37f0cc5e8b39387be35c4dea6a9979e6350c1
Sérgio Saquetim <saquetim@discourse.org>
2025-04-23 19:58:53 -0300

    Fix systems spec

--- spec/system/page_objects/pages/post_voting_topic.rb
+++ spec/system/page_objects/pages/post_voting_topic.rb
@@ -4,7 +4,7 @@ module PageObjects
   module Pages
     class PostVotingTopic < PageObjects::Pages::Topic
       COMMENT_VOTE_BUTTON = ".post-voting-comment-actions-vote button"
-      POST_VOTE_BUTTON = ".post-voting-post .post-voting-button-shim button"
+      POST_VOTE_BUTTON = ".post-voting-post button"
       COMMENT_ACTIONS = ".post-voting-comment-actions"
     end
   end
