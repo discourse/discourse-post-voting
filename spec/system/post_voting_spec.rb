8abfc54012664aa583e1592e0d0fca19b604e5aa
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2025-04-30 00:16:10 -0300

    DEV: Add compatibility with the Glimmer Post Stream (#244)

--- spec/system/post_voting_spec.rb
+++ spec/system/post_voting_spec.rb
@@ -7,23 +7,34 @@ RSpec.describe "Post voting", type: :system do
 
   let(:topic_page) { PageObjects::Pages::PostVotingTopic.new }
 
-  it "disallows voting on archived or closed topics" do
-    SiteSetting.post_voting_enabled = true
-    topic = Fabricate(:topic, subtype: Topic::POST_VOTING_SUBTYPE, user: user2, archived: true)
-    Fabricate(:post, topic: topic, post_number: 2)
-    post = Fabricate(:post, topic: topic, post_number: 3)
-    Fabricate(:post_voting_comment, post: post, user: user1)
+  %w[enabled disabled].each do |value|
+    before { SiteSetting.glimmer_post_stream_mode = value }
 
-    sign_in(user1)
+    context "when glimmer_post_stream_mode=#{value}" do
+      it "disallows voting on archived or closed topics" do
+        SiteSetting.post_voting_enabled = true
+        topic = Fabricate(:topic, subtype: Topic::POST_VOTING_SUBTYPE, user: user2, archived: true)
+        Fabricate(:post, topic: topic, post_number: 2)
+        post = Fabricate(:post, topic: topic, post_number: 3)
+        Fabricate(:post_voting_comment, post: post, user: user1)
 
-    topic_page.visit_topic(topic)
+        sign_in(user1)
 
-    expect(page).to have_css(PageObjects::Pages::PostVotingTopic::POST_VOTE_BUTTON, count: 4)
-    expect(page).to have_css(
-      "#{PageObjects::Pages::PostVotingTopic::POST_VOTE_BUTTON}:disabled",
-      count: 4,
-    )
-    expect(page).to have_css("#{PageObjects::Pages::PostVotingTopic::COMMENT_VOTE_BUTTON}:disabled")
-    expect(page).to have_no_css(PageObjects::Pages::PostVotingTopic::COMMENT_ACTIONS, visible: :all)
+        topic_page.visit_topic(topic)
+
+        expect(page).to have_css(PageObjects::Pages::PostVotingTopic::POST_VOTE_BUTTON, count: 4)
+        expect(page).to have_css(
+          "#{PageObjects::Pages::PostVotingTopic::POST_VOTE_BUTTON}:disabled",
+          count: 4,
+        )
+        expect(page).to have_css(
+          "#{PageObjects::Pages::PostVotingTopic::COMMENT_VOTE_BUTTON}:disabled",
+        )
+        expect(page).to have_no_css(
+          PageObjects::Pages::PostVotingTopic::COMMENT_ACTIONS,
+          visible: :all,
+        )
+      end
+    end
   end
 end
